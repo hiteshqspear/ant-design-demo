fc9457db813e7ecb7a7cdfca5ea46b0df06b7547
Prashant kumar <prashant.kumar@qspear.com>
2022-12-27 12:10:55 +0530

    Revert "test"
    This reverts commit 039062160d258db1aeb489587d7820d5ad372010.

--- ant-design-ui/src/components/Collapse/CollapsePanel.jsx
+++ ant-design-ui/src/components/Collapse/CollapsePanel.jsx
@@ -219,7 +219,6 @@ const CollapsePanel = () => {
                                     textAlign: 'right',
                                 }}
                             >
-                                {/* test */}
                                 <Button
                                     type="primary"
                                     htmlType="submit"
